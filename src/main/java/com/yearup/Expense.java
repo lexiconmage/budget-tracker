20ed74f5b01bb7502be5cd9bdc9698bc61df98e8
ryanpaynt <ryanpaynt@outlook.com>
2022-07-06 11:21:12 -0400

    created expense class

--- src/main/java/com/yearup/Expense.java
+++ src/main/java/com/yearup/Expense.java
@@ -3,7 +3,28 @@ package com.yearup;
 import java.util.Date;
 
 public class Expense extends Money {
-	public Expense() {
-		
+//	expenseAmount, expenseName, date
+	private float expenseAmount; 
+	private String expenseName; 
+	private Date date;
+	
+	public Expense(float expenseAmount, String expenseName, Date date) {
+		this.expenseAmount = expenseAmount;
+		this.expenseName = expenseName;
+		this.date = date;
+	}
+	
+	public void setExpenseAmount(float val) {
+		this.expenseAmount = val;
+	}
+	public void setExpenseName(String val) {
+		this.expenseName = val;
+	}
+	public float getExpenseAmount() {
+		return this.expenseAmount;
+	}
+	public String getExpenseName() {
+		return this.expenseName;
 	}
 }
+
